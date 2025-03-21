fcec9d57d50714ddc860ccd67fc0e22f3c47e866
Eduardo Gonzalez <eduardogs@usp.br>
2025-03-21 10:30:13 -0300

    Mudanca na frase otimisacao

--- lerolero.py
+++ lerolero.py
@@ -5,7 +5,7 @@ Gera frases de efeito comercial com sentido profundo."""
 
 import random
 
-parte1 = ["O sistema em desenvolvimento", "O novo protocolo de comunicação", "O algoritmo otimizado"]
+parte1 = ["O sistema em desenvolvimento", "O novo protocolo de comunicação", "O algoritmo foi otimizado e"]
 parte2 = ["Possui excelente desempenho", "Oferece garantias de precisão acima da média", "Preenche uma lacuna significativa"]
 parte3 = ["Nas aplicações a que se destina", "Em relação às opções disponíveis no mercado", "Promovendo amplas vantagens competitivas a seus usuários"]
 
