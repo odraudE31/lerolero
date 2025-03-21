180c2e5bdf2e80d5cda41a8941ce614a571eb374
Enzo <enzo.bizinoto@usp.br>
2025-03-21 09:57:32 -0300

    Agora tá certo bebezão

--- lerolero.py
+++ lerolero.py
@@ -3,3 +3,11 @@
 
 Gera frases de efeito comercial com sentido profundo."""
 
+import random
+
+parte1 = []
+parte2 = []
+parte3 = []
+
+print(random.choice(parte1), random.choice(parte2), random.choice(parte3))
+
