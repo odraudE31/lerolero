4f924947447af2fd919e74f8c7ba5073c78c3443
Enzo <enzo.bizinoto@usp.br>
2025-03-21 10:18:35 -0300

    Trechos gostosos das frases inseridos

--- lerolero.py
+++ lerolero.py
@@ -5,9 +5,12 @@ Gera frases de efeito comercial com sentido profundo."""
 
 import random
 
-parte1 = []
-parte2 = []
-parte3 = []
+parte1 = ["O sistema em desenvolvimento", "O novo protocolo de comunicação", "O algoritmo otimizado"]
+parte2 = ["Possui excelente desempenho", "Oferece garantias de precisão acima da média", "Preenche uma lacuna significativa"]
+parte3 = ["Nas aplicações a que se destina", "Em relação às opções disponíveis no mercado", "Promovendo amplas vantagens competitivas a seus usuários"]
+
 
 print(random.choice(parte1), random.choice(parte2), random.choice(parte3))
+
+
 
